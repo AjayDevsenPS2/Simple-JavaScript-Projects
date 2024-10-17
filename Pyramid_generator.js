020982976e559a4092536695aa43d07a410d6edd
Ajay Devsen P S <ajay2devsenps@gmail.com>
2024-10-17 22:12:58 +0530

    Added change for invertion also

--- Pyramid_generator.js
+++ Pyramid_generator.js
@@ -1,18 +1,18 @@
 const character = "#";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
-
-if ("") {
-  console.log("Condition is true");
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
 
 let result = ""
